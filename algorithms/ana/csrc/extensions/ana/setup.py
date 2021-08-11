d66adca4f26551727678d0336fa60320b4d72b87
Matteo Spallanzani <spmatteo@iis.ee.ethz.ch>
2021-08-11 15:40:18 +0200

    Enable compilation of normal noise

--- algorithms/ana/csrc/extensions/ana/setup.py
+++ algorithms/ana/csrc/extensions/ana/setup.py
@@ -26,7 +26,7 @@ setup(name='ana',
       ext_modules=[
           cpp_extension.CUDAExtension(name='ana_uniform_cuda',    sources=['uniform_cuda.cpp',    'uniform_cuda_kernel.cu']),
           cpp_extension.CUDAExtension(name='ana_triangular_cuda', sources=['triangular_cuda.cpp', 'triangular_cuda_kernel.cu']),
-          # cpp_extension.CUDAExtension(name='ana_normal_cuda',     sources=['normal_cuda.cpp',     'normal_cuda_kernel.cu']),
+          cpp_extension.CUDAExtension(name='ana_normal_cuda',     sources=['normal_cuda.cpp',     'normal_cuda_kernel.cu']),
           cpp_extension.CUDAExtension(name='ana_logistic_cuda',   sources=['logistic_cuda.cpp',   'logistic_cuda_kernel.cu']),
       ],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
