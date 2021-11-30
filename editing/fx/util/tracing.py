b1fe75ceacf882c6da671d870f1d16d78e50f2f9
Matteo Spallanzani <spmatteo@iis.ee.ethz.ch>
2021-11-30 15:11:23 +0100

    Implement an input check in TracerFactory; add docstrings and some comments

--- editing/fx/util/tracing.py
+++ editing/fx/util/tracing.py
@@ -34,16 +34,11 @@ __all__ = [
 ]
 
 
-_QL_modules = {
-    'ANA': [ANAModule],
-}
-
-
 class CustomTracer(fx.Tracer):
     """An ``fx.Tracer`` with custom granularity.
 
-    This class provides ``Tracer``s that interpret user-defined ``nn.Module``
-    classes as atomic ``fx.Node``s during symbolic tracing.
+    This class is the blueprint for ``Tracer``s that interpret user-defined
+    ``nn.Module`` classes as atomic ``fx.Node``s during symbolic tracing.
     """
 
     def __init__(self,
@@ -54,11 +49,16 @@ class CustomTracer(fx.Tracer):
         super().__init__(*args, **kwargs)
         self._leaf_types = () if leaf_types is None else tuple(leaf_types)
 
-    def is_leaf_module(self, m: torch.nn.Module, module_qualified_name: str):
+    def is_leaf_module(self, m: torch.nn.Module, module_qualified_name: str) -> bool:
         """Extend the base class check to custom ``nn.Module``s."""
         fxtracer_cond = super(CustomTracer, self).is_leaf_module(m, module_qualified_name)
         custom_cond = isinstance(m, self._leaf_types)
         return fxtracer_cond and custom_cond
+
+
+_QL_modules = {
+    'ANA': [ANAModule],
+}
 
 
 class TracerFactory(object):
@@ -69,14 +69,32 @@ class TracerFactory(object):
     @staticmethod
     def get_tracer(ql_algorithms: Union[str, List[str]],
                    *args,
-                   **kwargs):
+                   **kwargs) -> CustomTracer:
+        """Create a ``CustomTracer`` for the target QAT algorithms.
+
+        This function resolves strings (acronyms of QAT algorithms) into
+        collections of QuantLab ``nn.Module``s, then creates and returns a
+        ``CustomTracer`` which will trace such ``Module``s as atoms.
+
+        This function is part of ``quantlib``'s public interface. Therefore,
+        we want to provide a flexible way for users to specify the behaviour,
+        although this requires casting inputs to a valid form before the core
+        processing.
+        """
 
         # canonicalise input
         if isinstance(ql_algorithms, (str, list)):
             if isinstance(ql_algorithms, str):
                 ql_algorithms = [ql_algorithms]
+            ql_algorithms = [a.upper() for a in ql_algorithms]
+
         else:
             raise TypeError("TracerFactory expects str or List[str], but type {} was passed.".format(type(ql_algorithms)))
+
+        # validity
+        wrong_keys = set(ql_algorithms).difference(set(_QL_modules.keys()))
+        if len(wrong_keys) > 0:
+            raise ValueError("QuantLab does not support algorithms {}.".format(wrong_keys))
 
         # retrieve the ``nn.Module``s to be traced as atoms
         ql_modules = list(itertools.chain(*[_QL_modules[k] for k in ql_algorithms]))
@@ -86,11 +104,14 @@ class TracerFactory(object):
 def custom_symbolic_trace(root: Union[Callable, nn.Module],
                           concrete_args: Optional[Dict[str, Any]] = None,
                           enable_cpatching: bool = False,
-                          tracer: Optional[fx.Tracer] = None):
+                          tracer: Optional[fx.Tracer] = None) -> fx.GraphModule:
+    """Extend ``fx.symbolic_trace`` to tracings with custom granularity."""
 
+    # resolve to PyTorch's "standard library"
     if tracer is None:
         gm = fx.symbolic_trace(root, concrete_args=concrete_args, enable_cpatching=enable_cpatching)
 
+    # use ``CustomTracer``s to treat specific ``Module``s as atoms during symbolic tracing
     else:
         graph = tracer.trace(root, concrete_args)
         name = root.__class__.__name__ if isinstance(
