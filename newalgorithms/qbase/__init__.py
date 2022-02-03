3a3ddd824f76d79c764026ebf8399178dbf9a4e5
Matteo Spallanzani <spmatteo@iis.ee.ethz.ch>
2022-02-03 15:40:36 +0100

    Update the namespace of the qbase sub-package

--- newalgorithms/qbase/__init__.py
+++ newalgorithms/qbase/__init__.py
@@ -1,4 +1,4 @@
-from .qrange import QRange, UNSPECIFIED_OFFSET, IMPLICIT_STEP, resolve_qrangespec
+from .qrange import QRange, UNKNOWN, IMPLICIT_STEP, QRangeSpec, resolve_qrangespec
 from .qspecs import make_broadcastable, UNSPECIFIED_ZEROPOINT, UNSPECIFIED_SCALE, QGranularity, NON_BROADCAST_DIM, init_qhparams
 from .qclipper import get_zero_scale, get_scale, get_clipping_bounds
 from .observer import MinMaxMeanVarObserver
