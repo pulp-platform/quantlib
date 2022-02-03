78409212f5ca146ae782498a2856168a630bc9f1
Matteo Spallanzani <spmatteo@iis.ee.ethz.ch>
2022-02-03 15:04:35 +0100

    Untrack renamed file

--- newutils/errors.py
+++
@@ -1,9 +0,0 @@
-def quantlib_err_msg(class_name: str = "") -> str:
-    """Create a header for QuantLib error messages.
-
-    Arguments:
-        class_name: the (optional) class name of the object that triggered the
-            error.
-
-    """
-    return "[QuantLib Error] " + (f"{class_name}: " if class_name != "" else class_name)
